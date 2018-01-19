99ac174dc483cf0be4e6b18d83cb1c2726b828a5
Bryce Frazier <bx.frazier@gmail.com>
2018-01-19 08:48:54 -0800

    add some js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,18 @@
+//numbers with a 0 are replaced with "Beep!"
+//numbers that contain a 1 are replaced with "Boop!"
+//numbers that are divisible by 3 are replaced with "I'm sorry, Dave. I'm afraid I can't do that."
 
+//back-end logic
+var userNumber = function(userInput) {
+var numberArray = ['0', '1', '3'];
+}
+  if (userNumber.includes()
+
+
+//fron-end logic
+$(document).ready(function() {
+  $("form#compute").submit(function(event) {
+      event.preventDefault();
+
+  })
+})
